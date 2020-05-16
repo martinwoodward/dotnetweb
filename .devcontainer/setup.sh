0a16a368a7d60b230f39690df7fd29fb1305c287
Martin Woodward <martin@woodwardweb.com>
2020-05-16 22:38:50 +0100

    Set tzdata for Ubuntu 20.04

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,6 @@
+# set noninteractive installation
+export DEBIAN_FRONTEND=noninteractive
+
 ## update and install some things we should probably have
 apt-get update
 apt-get install -y \
@@ -6,11 +9,17 @@ apt-get install -y \
   gnupg2 \
   jq \
   sudo \
+  tzdata \
   zsh
 
+# set your timezone
+ln -fs /usr/share/zoneinfo/UCT /etc/localtime
+#ln -fs /usr/share/zoneinfo/GB /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
 ## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#cp -R /root/.oh-my-zsh /home/$USERNAME
+#cp /root/.zshrc /home/$USERNAME
+#sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+#chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
