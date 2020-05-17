54b3f4952016a270ffb26e5423f4393ce3a00a7b
Martin Woodward <martin@woodwardweb.com>
2020-05-17 14:00:11 +0100

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
-var i=0;
+function escapeHTML(text) {  
+    var replacements= {"<": "&lt;", ">": "&gt;","&": "&amp;", """: "&quot;"};                      
+    return text.replace(/[<>&"]/g, function(character) {  
+        return replacements[character];  
+    }); 
+}
