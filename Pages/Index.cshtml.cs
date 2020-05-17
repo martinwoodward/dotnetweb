2a6804d13e83e421b4b1b2e2835e5288dcf62994
Martin Woodward <martin@woodwardweb.com>
2020-05-17 23:57:23 +0000

    Update messages

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,7 +14,7 @@ namespace dotnetweb.Pages
         public string[] Messages {
             get {
                 return new string[] {
-                    "Hello World!",
+                    "Hello Build!",
                     "Would you like a Cup<T>?",
                     ".NET all the things!",
                     "Would you like to play a game?"
