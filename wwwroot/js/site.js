95d01ff494c40faef2269879533a344572888142
Martin Woodward <martin@woodwardweb.com>
2020-05-17 14:15:11 +0100

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,2 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-function escapeHTML(text) {  
-    var replacements= {"<": "&lt;", ">": "&gt;","&": "&amp;", """: "&quot;"};                      
-    return text.replace(/[<>&"]/g, function(character) {  
-        return replacements[character];  
-    }); 
-}
+// Stubbed bit of Javscript to see if minification working
+var noOp = 0;
