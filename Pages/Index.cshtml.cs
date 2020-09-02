fd4c1e14f5d40fc149b29e0ef005141fc7340610
Martin Woodward <martinwoodward@github.com>
2020-09-02 17:36:00 +0000

    Fix messages

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,7 +15,7 @@ namespace dotnetweb.Pages
         public string[] Messages {
             get {
                 return new string[] {
-                    "Hallo Wereld!",
+                    "Hello MVP's!",
                     "Would you like a Cup<T>?",
                     ".NET all the things!",
                     "Would you like to play a game?"
